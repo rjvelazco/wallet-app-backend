abb4bcc8a9938d6097b876dc1cdf2db15609fa77
rjvelazco <rjvelazco21@gmail.com>
2021-06-09 21:25:09 -0400

    Static page

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 const app = express();
+
+
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.listen(process.env.PORT, ()=>{
     console.log('Servidor online: ', process.env.PORT);
